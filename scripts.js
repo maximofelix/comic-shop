9600eb453a54cbfd8b60229b1ee704e433ad88d9
maximofelix <maximofelix@gmail.com>
2023-04-14 23:52:52 -0300

    Checkin Inicial
    Checkin inicial do projeto.

---
+++ scripts.js
@@ -0,0 +1,201 @@
+// Lê o localStorage e se não existir a entrada, cria uma lista vazia
+const pessoas = JSON.parse(localStorage.getItem("usuarios") || [])
+
+function atualizar(){
+    console.log('atualizar()')
+    if (pessoas == null)
+        return;
+    const tabela = document.getElementById("tabUsuarios");
+    pessoas.forEach(pessoa => {    
+        tabela.appendChild(criarLinha(pessoa));
+    });
+}
+
+function carregar(){
+    console.log('carregar()')
+    const query = window.location.search;
+    const param = new URLSearchParams(query);
+    const _login = param.get('login');
+    const _opera = param.get('op');
+    
+    console.log('usuário informado..: ' + _login)
+    
+    if (_login == null){ 
+        return;
+    }
+
+    var pessoa = null;
+    pessoas.forEach(pes => {
+        if (pes.login == _login)
+            pessoa = pes
+    });
+
+    const btn = document.getElementById('btnCadastrar');
+    if (pessoa == null) {
+        //alert(`usuário [${_login}] não localizado`)
+        //return;
+        pessoa = new Pessoa('','', null, 'M', '', _login, ''); 
+    }
+    else{
+        btn.innerHTML = 'Alterar'
+    }
+    console.log('entrou')
+    console.log(pessoa.login)
+    carregarPessoa(pessoa)
+}
+
+function carregarPessoa(pessoa){    
+    console.log('carregarPessoa()')
+    document.getElementById("txtNome").value = pessoa.nome;
+    document.getElementById("txtEmail").value = pessoa.email;
+    document.getElementById("txtDataNasc").value = pessoa.dataNasc;
+    document.getElementById("cbxSexo").value = pessoa.sexo;
+    document.getElementById("txtEndereco").value = pessoa.endereco;
+    document.getElementById("txtLogin").value = pessoa.login;
+    document.getElementById("txtSenha").value = pessoa.senha;
+}
+
+function criarLinha(pessoa){
+    console.log('criarlinha()')
+    const linha = document.createElement("tr");
+    const tdlogin = document.createElement("td");
+    tdlogin.setAttribute("id", pessoa.login);
+
+    const link = document.createElement('a');
+    link.href = "./cadastro/?login=" + pessoa.login;
+    link.target = '_top'
+    link.innerHTML = pessoa.login;
+    tdlogin.appendChild(link)
+    linha.appendChild(tdlogin);
+    
+    const tdnome = document.createElement("td");
+    tdnome.innerHTML = pessoa.nome;
+    linha.appendChild(tdnome);
+    const tdsexo = document.createElement("td");
+    tdsexo.innerHTML = pessoa.sexo;
+    linha.appendChild(tdsexo);
+    const tdemail = document.createElement("td");
+    tdemail.innerHTML = pessoa.email;
+    linha.appendChild(tdemail);
+    const tddatanasc = document.createElement("td");
+    tddatanasc.innerHTML = pessoa.dataNasc;
+    linha.appendChild(tddatanasc);
+    linha.appendChild(tdemail);
+
+    const tdExcluir = document.createElement("td");
+    const btnExcluir = document.createElement('button');
+    btnExcluir.name = 'btnExcluir'
+    btnExcluir.type = 'button'
+    btnExcluir.innerHTML = 'excluir'
+    btnExcluir.onclick = function(){ excluir(pessoa.login) }
+    tdExcluir.appendChild(btnExcluir)
+    linha.appendChild(tdExcluir);
+    return linha
+}
+
+class Pessoa  {
+    constructor(nome, email, dataNasc, sexo, endereco, login, senha){
+        this.nome = nome;
+        this.email = email;
+        this.dataNasc = dataNasc;
+        this.sexo = sexo;
+        this.endereco = endereco;
+        this.login = login;
+        this.senha = senha;
+    }
+}
+
+
+function cadastrar() {
+    console.log('cadastrar()')
+    const pessoa  = {
+        nome : document.getElementById("txtNome").value,
+        email : document.getElementById("txtEmail").value,
+        dataNasc : document.getElementById("txtDataNasc").value,
+        sexo : document.getElementById("cbxSexo").value,
+        endereco : document.getElementById("txtEndereco").value,
+        login : document.getElementById("txtLogin").value,
+        senha : document.getElementById("txtSenha").value
+    }
+     
+    //document.getElementById(pessoa.login)
+
+    if (pessoa.login.trim() == "") {
+        alert("Login é obrigatório");
+    }
+    else if (pessoa.login.includes(" ")) {
+        alert("Login não pode ter espaços");
+    }
+    else if (pessoa.nome.trim() == "") {
+        alert("Nome é obrigatório");
+    }
+    //else if (pessoas.indexOf(pessoa.login) > 0) {
+    //    alert("login já cadastrado");
+    //}
+    else {
+        
+        // Excluir se já existir
+        const index = pessoas.findIndex((pes) => pes.login ==  pessoa.login);
+        if (index >= 0){
+            const x = pessoas.splice(index, 1);
+        }
+        // Adicionar
+        pessoas.push(pessoa);
+        // grava no armazenamento
+        localStorage.setItem("usuarios", JSON.stringify(pessoas))
+
+        //const tabela = document.getElementById("tabUsuarios");
+
+        /*const linha = document.createElement("tr");
+        const tdlogin = document.createElement("td");
+        tdlogin.setAttribute("id", pessoa.login);
+        tdlogin.innerHTML = pessoa.login;
+        linha.appendChild(tdlogin);
+        
+        const tdnome = document.createElement("td");
+        tdnome.innerHTML = pessoa.nome;
+        linha.appendChild(tdnome);
+        const tdsexo = document.createElement("td");
+        tdsexo.innerHTML = pessoa.sexo;
+        linha.appendChild(tdsexo);
+        const tdemail = document.createElement("td");
+        tdemail.innerHTML = pessoa.email;
+        linha.appendChild(tdemail);
+        const tddatanasc = document.createElement("td");
+        tddatanasc.innerHTML = pessoa.dataNasc;
+        linha.appendChild(tddatanasc);*/
+
+        //tabela.appendChild(criarLinha(pessoa))
+
+        alert("Usuário cadastrado com sucesso!");
+    }
+    window.location.href = "./consulta.html";
+}
+
+function excluir(login){
+    const index = pessoas.findIndex((pes) => pes.login ==  login);
+    if (index >= 0){
+        const x = pessoas.splice(index, 1);
+        localStorage.setItem("usuarios", JSON.stringify(pessoas))
+        alert('[' + login + '] excluído com sucesso')
+    }
+}
+
+function testarApi() {
+    console.log('testarApi()')
+    const fillList = document.getElementById("fillList");
+    // fetch('https://jsonplaceholder.typicode.com/todos/', method: 'GET');
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then(data => {
+            // obtem a lista (html)
+            const list = document.querySelector('#fillList')
+            // para cada item do retorno json
+            data.map((item) => {
+                const li = document.createElement('li')
+                li.setAttribute('id', item.id);
+                li.innerHTML = item.title;
+                list.appendChild(li);
+            })
+        })
+}
